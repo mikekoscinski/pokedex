820eae73fd88dd02702916d26f1d1c9a1de71dcd
Michael Koscinski <michael.koscinski@gmail.com>
2020-11-06 15:09:23 -0500

    Remove filterTableElements() and other vestiges of original DOM-based browser approach to scraping (before I realized document.querySelector would not work server-side)

--- scrape.js
+++ scrape.js
@@ -3,14 +3,11 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-const pokemonURLPairsToScrape = [];
-const URLsForScraper = [];
-const moveTableElements = [];
-const allMovesForAnIndividualPokemon = [];
-const methodsMovesAreObtained = ['level up', 'egg', 'hm', 'tm'];
 const allPokemonNames = [
 	"Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran-f", "Nidorina", "Nidoqueen", "Nidoran-m", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetchd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr-Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew", "Chikorita", "Bayleef", "Meganium", "Cyndaquil", "Quilava", "Typhlosion", "Totodile", "Croconaw", "Feraligatr", "Sentret", "Furret", "Hoothoot", "Noctowl", "Ledyba", "Ledian", "Spinarak", "Ariados", "Crobat", "Chinchou", "Lanturn", "Pichu", "Cleffa", "Igglybuff", "Togepi", "Togetic", "Natu", "Xatu", "Mareep", "Flaaffy", "Ampharos", "Bellossom", "Marill", "Azumarill", "Sudowoodo", "Politoed", "Hoppip", "Skiploom", "Jumpluff", "Aipom", "Sunkern", "Sunflora", "Yanma", "Wooper", "Quagsire", "Espeon", "Umbreon", "Murkrow", "Slowking", "Misdreavus", "Unown", "Wobbuffet", "Girafarig", "Pineco", "Forretress", "Dunsparce", "Gligar", "Steelix", "Snubbull", "Granbull", "Qwilfish", "Scizor", "Shuckle", "Heracross", "Sneasel", "Teddiursa", "Ursaring", "Slugma", "Magcargo", "Swinub", "Piloswine", "Corsola", "Remoraid", "Octillery", "Delibird", "Mantine", "Skarmory", "Houndour", "Houndoom", "Kingdra", "Phanpy", "Donphan", "Porygon2", "Stantler", "Smeargle", "Tyrogue", "Hitmontop", "Smoochum", "Elekid", "Magby", "Miltank", "Blissey", "Raikou", "Entei", "Suicune", "Larvitar", "Pupitar", "Tyranitar", "Lugia", "Ho-oh", "Celebi", "Treecko", "Grovyle", "Sceptile", "Torchic", "Combusken", "Blaziken", "Mudkip", "Marshtomp", "Swampert", "Poochyena", "Mightyena", "Zigzagoon", "Linoone", "Wurmple", "Silcoon", "Beautifly", "Cascoon", "Dustox", "Lotad", "Lombre", "Ludicolo", "Seedot", "Nuzleaf", "Shiftry", "Taillow", "Swellow", "Wingull", "Pelipper", "Ralts", "Kirlia", "Gardevoir", "Surskit", "Masquerain", "Shroomish", "Breloom", "Slakoth", "Vigoroth", "Slaking", "Nincada", "Ninjask", "Shedinja", "Whismur", "Loudred", "Exploud", "Makuhita", "Hariyama", "Azurill", "Nosepass", "Skitty", "Delcatty", "Sableye", "Mawile", "Aron", "Lairon", "Aggron", "Meditite", "Medicham", "Electrike", "Manectric", "Plusle", "Minun", "Volbeat", "Illumise", "Roselia", "Gulpin", "Swalot", "Carvanha", "Sharpedo", "Wailmer", "Wailord", "Numel", "Camerupt", "Torkoal", "Spoink", "Grumpig", "Spinda", "Trapinch", "Vibrava", "Flygon", "Cacnea", "Cacturne", "Swablu", "Altaria", "Zangoose", "Seviper", "Lunatone", "Solrock", "Barboach", "Whiscash", "Corphish", "Crawdaunt", "Baltoy", "Claydol", "Lileep", "Cradily", "Anorith", "Armaldo", "Feebas", "Milotic", "Castform", "Kecleon", "Shuppet", "Banette", "Duskull", "Dusclops", "Tropius", "Chimecho", "Absol", "Wynaut", "Snorunt", "Glalie", "Spheal", "Sealeo", "Walrein", "Clamperl", "Huntail", "Gorebyss", "Relicanth", "Luvdisc", "Bagon", "Shelgon", "Salamence", "Beldum", "Metang", "Metagross", "Regirock", "Regice", "Registeel", "Latias", "Latios", "Kyogre", "Groudon", "Rayquaza", "Jirachi", "Deoxys",
 ];
+const URLsForScraper = [];
+const pokemonURLPairsToScrape = [];
 
 function generateURLsForScraper () {
 	allPokemonNames.forEach(pokemon => {
@@ -23,25 +20,10 @@ function createPokemonURLPairs () {
 	allPokemonNames.forEach(pokemon => {
 		const newPokemonToScrape = { 
 			name: pokemon,
-			url: URLsForScraper.find(url => url.includes(pokemon)) 
+			url: URLsForScraper.find(url => url.includes(pokemon)),
 		}
 		pokemonURLPairsToScrape.push(newPokemonToScrape);
 	});	
-}
-
-// Select table elements to eventually be processed
-function filterTableElements () {
-	const h3Elements = $('h3').html();
-	h3Elements.forEach(h3Element => {
-		methodsMovesAreObtained.some(moveCategory => {
-			const isMoveTableElement = Boolean(h3Element.innerText.toLowerCase().includes(moveCategory)).valueOf();
-			const isEmeraldVersion = Boolean(h3Element.nextElementSibling.innerText.toLowerCase().includes('emerald')).valueOf();
-			if (!isMoveTableElement) return;
-			if (!isEmeraldVersion) return;
-			const h3SiblingDataTable = h3Element.nextElementSibling.nextElementSibling.firstElementChild.querySelector('tbody');
-			moveTableElements.push(h3SiblingDataTable);
-		});
-	});
 }
 
 function retrieveMoves () {
@@ -77,19 +59,6 @@ function prepareScraper() {
 	createPokemonURLPairs();
 }
 
-// function runScraper (name) {
-// 	// TODO: Will need to call these forEach pokemon in allPokemon
-// 	retrieveMoves();
-// 	filterTableElements();
-// 	retrieveMoves();
-// }
-
-// TODO: Call runScraper forEach pokemon in allPokemon
-// runScraper('Nidoking');
-
-// TODO: Add the following to do it for each pairing:
-// pokemonURLPairsToScrape.forEach(pokemon => {
-
 function runScraper (pokemon) {
 	axios.get(pokemon.url)
 		.then(function (response) {
@@ -106,6 +75,8 @@ function runScraper (pokemon) {
 			const preEvolutionMoves = emeraldMovesTab.children().first().next().next().next().next().next().next().next().next().next().next().next().children().children().last();
 			const hmMoves = emeraldMovesTab.next().children().first().next().next().children().children().last();
 			const tmMoves = emeraldMovesTab.next().children().first().next().next().next().next().next().children().children().last();
+			
+			console.log(tmMoves.text());
 			
 			// TODO: Write one function to process level-up (grabs level_learned, move_name) and another for everything else (grabs move_name)
 			// grab the first cell-num and store it as level
@@ -129,19 +100,11 @@ function runScraper (pokemon) {
 	);
 }
 
-
-
 const nidokingTestObject = {
 	name: 'Nidoking',
 	url: 'https://pokemondb.net/pokedex/nidoking/moves/3',
 }
 
 prepareScraper();
-// runScraper(nidokingTestObject);
-
-runScraper(pokemonURLPairsToScrape);
-
-// TODO: Use this to run scraper for all pokemon, once logic is correct
-// pokemonURLPairsToScrape.forEach(pokemon => {
-// 	runScraper(pokemon);
-// });
+// pokemonURLPairsToScrape.forEach(pokemon => runScraper(pokemon));
+runScraper(nidokingTestObject);
