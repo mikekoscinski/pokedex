670c31d5e09ca0f478b67d83ede7639744752f12
Michael Koscinski <michael.koscinski@gmail.com>
2020-11-16 16:56:24 -0500

    Save production SQL execution files

--- database/generation.sql
+++ database/generation.sql
@@ -1,6 +1,6 @@
-CREATE TABLE "Generation" (
+CREATE TABLE "Generation" ( 
 	id INTEGER PRIMARY KEY NOT NULL UNIQUE 
 );
-INSERT INTO Generation (id) VALUES (1);
-INSERT INTO Generation (id) VALUES (2);
-INSERT INTO Generation (id) VALUES (3);
+INSERT INTO "Generation" (id) VALUES (1);
+INSERT INTO "Generation" (id) VALUES (2);
+INSERT INTO "Generation" (id) VALUES (3);
--- database/region.sql
+++ database/region.sql
@@ -1,6 +1,6 @@
 CREATE TABLE "Region" (
 	id VARCHAR(255) PRIMARY KEY NOT NULL UNIQUE 
 );
-INSERT INTO Region (id) VALUES (Kanto);
-INSERT INTO Region (id) VALUES (Johto);
-INSERT INTO Region (id) VALUES (Hoenn);
+INSERT INTO "Region" (id) VALUES ('Kanto');
+INSERT INTO "Region" (id) VALUES ('Johto');
+INSERT INTO "Region" (id) VALUES ('Hoenn');
