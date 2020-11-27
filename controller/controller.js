3cdc917af3848a6d4cf4cd5d9545bc2ac0c6708b
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2020-11-27 11:31:04 -0500

    Rename index.js as controller.js; install cors and express in controller folder

---
+++ controller/controller.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const pokemonController = require('../model/pokemon-model');
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// ROUTES (RESTful API with Postgres):
+// Get all pokemon (for index page)
+app.get('/pokemon', pokemonController.index);
+
+// Get a specific pokemon (for individual pokedex entry page)
+app.get('/pokemon/:name', pokemonController.entry); // TODO: This is currently case-sensitive; must pass 'Bulbasaur' as param; 'bulbasaur' as lowercase will return nothing
+
+app.listen(5000, () => {
+	console.log('Server has started on port 5000');
+});
