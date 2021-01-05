9a8b4ba7e845d9df9559651505ff9159390bc885
Michael Koscinski <michael.koscinski@gmail.com>
2021-01-05 11:33:36 -0500

    Move App logic to App.js; prepare index.js to store all import statements - every import should be routed via index.js

--- view/src/App.js
+++ view/src/App.js
@@ -1,12 +1,10 @@
-// TODO: Make a single-page app, with 'view' and 'menu'; 'view' updated via state? This should just be the view? Menu is a separate component; rendered in index.js
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './Router';
 
-// TODO: I don't think this does anything. I moved the render logic to index.js
-
-// import React from "react";
-// import EntryList from "./EntryList";
-
-// export default function App() {
-//   return (
-//     <EntryList />
-//   );
-// };
+render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
