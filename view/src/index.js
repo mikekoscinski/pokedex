258651ec1237c6ecbd0a5b803082aac924352737
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2021-01-05 11:33:36 -0500

    Move App logic to App.js; prepare index.js to store all import statements - every import should be routed via index.js

--- view/src/index.js
+++ view/src/index.js
@@ -1,10 +1,1 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Router from './Router';
-
-render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// TODO: All of my imports should go here, so I can access everything from one place
