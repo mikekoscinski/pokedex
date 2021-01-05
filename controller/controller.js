a0573a3c563280ba62e61cfc70f992800b18450b
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2021-01-05 12:22:19 -0500

    Update controller.js test case

--- controller/controller.js
+++ controller/controller.js
@@ -16,16 +16,32 @@ router.get('/', async (req, res) => {
 });
 
 
+
+
+
+
+
+
+
+
 ////////////////////////////////////////////////////////////
 
 router.get('/pokemon', async (req, res) => {
 	try {
 		const { rows } = await model.getIndexData();
-		rows.forEach(el => console.log(el));
+
+		console.log('test');
+
+		// TODO: I need to somehow use rows.forEach(el => renderView(el));
 		
 		// TODO: Render view. I think I need to call a renderView function with 'rows' passed as a paremeter (then, in the React component file, call .forEach on 'rows' to render each row in the component)
 		res.send(rows);
 		// res.render('index');
+		
+		
+		
+		// TODO: Store 'rows' in dictionary
+		// don't do an array of objects here
 		
 		
 	} catch (error) {
@@ -34,6 +50,16 @@ router.get('/pokemon', async (req, res) => {
 });
 
 ////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
 
 router.get('/pokemon/:name', async (req, res) => {
 	try {
