db5980d43281a2baff2d4bacdbef4c457ac7416a
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2021-01-05 12:21:33 -0500

    Remove unused script tag from index.html (webpack handles this in create-react-app); move render logic back into index.js from App.js for now (it is the first module read by webpack)

--- view/src/index.js
+++ view/src/index.js
@@ -1,1 +1,36 @@
-// TODO: All of my imports should go here, so I can access everything from one place
+// // TODO: All of my imports should go here, so I can access everything from one place
+
+// import React from 'react'; // Must import React, even if not calling anything from React, because using JSX
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+// // Components:
+// import Login from './Login';
+// import EntryList from './EntryList';
+// import Entry from './Entry';
+// import Search from './Search';
+// import Teams from './Teams';
+// import MyAccount from './MyAccount';
+// import NotFound from './NotFound';
+
+// export {
+//   Login, 
+//   EntryList,
+//   Entry,
+//   Search,
+//   Teams,
+//   MyAccount,
+//   NotFound
+// }
+
+// NOTE: create-react-app is configured to use src/index.js as entry point. So, that's the first module it reads. Source: https://stackoverflow.com/questions/42438171/wheres-the-connection-between-index-html-and-index-js-in-a-create-react-app-app
+
+import React from 'react';
+import { render } from 'react-dom';
+import Router from './Router';
+
+render(
+  <React.StrictMode>
+    <Router />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
