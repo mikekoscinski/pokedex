9bfee7d0b9b20f073fc15d184722f0a2aae5ccab
Michael Koscinski <michael.koscinski@gmail.com>
2021-01-11 12:30:19 -0500

    Create live pagelinks in Navmenu; remove BrowserRouter and its methods from Navmenu (they weren't being used)

--- view/src/Navmenu.js
+++ view/src/Navmenu.js
@@ -1,20 +1,16 @@
 import React from 'react'
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-
-// TODO: Link in Navmenu Routes is not live yet
+// Note: Removed BrowserRouter; just use <a href>
 
 export default function Navmenu() {
 	return (
-		<BrowserRouter>
 			<div className="navmenu" key="navmenu">
 				<h1>Navmenu</h1>
 				<ul className="navmenu-items">
-					<Route><li className="navmenu-item"><Link to={'/pokemon'}></Link>Pokemon</li></Route>
-					<Route><li className="navmenu-item"><Link to={'/search'}></Link>Search</li></Route>
-					<Route><li className="navmenu-item"><Link to={'/teams'}></Link>Teams</li></Route>
-					<Route><li className="navmenu-item"><Link to={'/myaccount'}></Link>Account</li></Route>
+					<li><a href="/pokemon">Pokemon</a></li>
+					<li><a href="/search">Search</a></li>
+					<li><a href="/teams">Teams</a></li>
+					<li><a href="/myaccount">Account</a></li>
 				</ul>
 			</div>
-		</BrowserRouter>
 	)
 };
