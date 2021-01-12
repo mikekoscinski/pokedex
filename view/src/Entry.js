e3aa7371473dfb321279dd55dcee18929183aeed
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2021-01-12 12:29:00 -0500

    Update key values for children of siblings produced via .map()

--- view/src/Entry.js
+++ view/src/Entry.js
@@ -59,68 +59,72 @@ export default function Entry ({ match }) {
 	}, []);
 
 	return (
-		<div className="pokedex-entry">
+		<div className="pokedex-entry" key={'pokedex-entries'}>
 			{entry.map(entry => (
-				<div key={`pokedex-entry-${entry.name}-${entry.pokedex_id}`}>
-					<h1>{`${entry.name}`}</h1>
-					<h3>{`No. ${entry.pokedex_id}`}</h3>
-					<div className="bio-section">
-						<h2>Bio</h2>
-						<div className="bio-values">
-							<div className="bio-entry"><p>Region: <span className="bio-value">{entry.region_id}</span></p></div>
-							<div className="bio-entry"><p><span className="bio-value type">{entry.primary_type_id} <span className="bio-value type">{entry.secondary_type_id}</span></span></p></div>
+				<div key={`pokedex-entry-${entry.name.replace(/\s/g, '-').toLowerCase()}`}>
+					<h1 className="entry-name" key={'entry-name'}>{entry.name}</h1>
+					<h3 className="entry-number" key={'entry-number'}>{`No. ${entry.pokedex_id}`}</h3>
+					<div className="bio-section" key={'bio'}>
+						<h2 key={'h2'}>Bio</h2>
+						<div className="bio-values" key={'values'}>
+							<div className="bio-entry" key={'region'}><p>Region: <span className="bio-value">{entry.region_id}</span></p></div>
+							<div className="bio-entry" key={'types'}><p><span className="bio-value type">{entry.primary_type_id} <span className="bio-value type">{entry.secondary_type_id}</span></span></p></div>
 							{/* TODO: Once bios are in DB. May need different div structure for bio; should span entire page, vs table-like underlined flexbox appearance  */}
-							<p>TODO: INSERT BIO HERE.</p>
+							<div className="biography" key={'biography'}><p>TODO: INSERT BIO HERE.</p></div>
 						</div>
 					</div>
-					<div className="stats-section">
-						<h2>Stats</h2>
-						<div className="stat-values">
-							<div className="stat-entry"><p>Attack: <span className="stat-value">{entry.attack}</span></p></div>
-							<div className="stat-entry"><p>Defense: <span className="stat-value">{entry.defense}</span></p></div>
-							<div className="stat-entry"><p>Special Attack: <span className="stat-value">{entry.special_attack}</span></p></div>
-							<div className="stat-entry"><p>Special Defense: <span className="stat-value">{entry.special_defense}</span></p></div>
-							<div className="stat-entry"><p>Speed: <span className="stat-value">{entry.speed}</span></p></div>
-							<p>---</p>
-							<div className="stat-entry"><p>Average Stat: <span className="stat-value">{entry.average_stat}</span></p></div>
-							<div className="stat-entry"><p>Total Stats: <span className="stat-value">{entry.total_stats}</span></p></div>
+					<div className="stats-section" key={'stats'}>
+						<h2 key={'h2'}>Stats</h2>
+						<div className="stat-values" key={'stat-values'}>
+							<div className="stat-entry" key={'attack'}><p>Attack: <span className="stat-value">{entry.attack}</span></p></div>
+							<div className="stat-entry" key={'defense'}><p>Defense: <span className="stat-value">{entry.defense}</span></p></div>
+							<div className="stat-entry" key={'special-attack'}><p>Special Attack: <span className="stat-value">{entry.special_attack}</span></p></div>
+							<div className="stat-entry" key={'special-defense'}><p>Special Defense: <span className="stat-value">{entry.special_defense}</span></p></div>
+							<div className="stat-entry" key={'speed'}><p>Speed: <span className="stat-value">{entry.speed}</span></p></div>
+							<div className="stat-entry" key={'average'}><p>Average Stat: <span className="stat-value">{entry.average_stat}</span></p></div>
+							<div className="stat-entry" key={'total'}><p>Total Stats: <span className="stat-value">{entry.total_stats}</span></p></div>
 						</div>
 					</div>
-					<div className="moves-section">
-						<h2>Moves</h2>
+					<div className="moves-section" key={"moves"}>
+						<h2 key={'h2'}>Moves</h2>
 						{Array.from(new Set(entryMoves.map(move => move.method_obtained_id))).map(methodObtained => (
-							<div key={`${entry.name}-${methodObtained.replace(/\s/g, '')}-moves`}>
-								<h3 key={methodObtained}>{methodObtained}</h3>
-								<table>
-									<thead>
-										<tr>
-											<th>Move</th>
+							<div key={`${methodObtained.replace(/\s/g, '-').toLowerCase()}-moves`}>
+								<h3 key={'h3'}>{methodObtained}</h3>
+								<table key={'table'}>
+									<thead key={'table-heading'}>
+										<tr key={'table-heading-row'}>
+											<th key={'move'}>Move</th>
 											{/* https://reactjs.org/docs/conditional-rendering.html */}
-											{(methodObtained === 'Level up') ? <th>Level Obtained</th> : null}
-											<th>Type</th>
-											<th>Category</th>
-											<th>Power</th>
-											<th>Accuracy</th>
-											<th>PP</th>
-											<th>Effect</th>
+											{(methodObtained === 'Level up') ? <th key={'level-obtained'}>Level Obtained</th> : null}
+											<th key={'type'}>Type</th>
+											<th key={'category'}>Category</th>
+											<th key={'power'}>Power</th>
+											<th key={'accuracy'}>Accuracy</th>
+											<th key={'pp'}>PP</th>
+											<th key={'effect'}>Effect</th>
 										</tr>
 									</thead>
-									<tbody>
+									<tbody key={'table-body'}>
 										{entryMoves.filter(move => move.method_obtained_id === methodObtained).map(move => (
-											<tr key={`${move.move_id.replace(/\s/g, '-').toLowerCase()}-${move.method_obtained_id.replace(/\s/g, '-').toLowerCase()}${move.level_obtained_id ? `-level-${move.level_obtained_id}` : null}`}>
-												<td>{move.move_id}</td>
-												{(methodObtained === 'Level up') ? <td>{move.level_obtained_id}</td> : null}
-												{/* TODO: 
-														moveInfo.map(move => (
-															<td> for each property of the move
-														))
-												 */}
-												<td>Type</td>
-												<td>Category</td>
-												<td>Power</td>
-												<td>Accuracy</td>
-												<td>PP</td>
-												<td>Effect</td>
+											<tr 
+												key={`${move.move_id.replace(/\s/g, '-').toLowerCase()}-${move.level_obtained_id ? `level-${move.level_obtained_id}` : move.method_obtained_id.replace(/\s/g, '-').toLowerCase()}-entry`}
+											>
+												<td key={`move-id`}>{move.move_id}</td>
+												{/* IF level up, create level_obtained <td>. Ternary is React's only in-line IF statement option */}
+												{(methodObtained === 'Level up') ?
+													<td key={`level-obtained`}>{move.level_obtained_id}</td> 
+													: null
+												}
+												{entryMovesInfo.filter(moveInfo => moveInfo.id === move.move_id).map(moveInfo => (
+													<>
+													<td key={`type`}>{moveInfo.type_id}</td>
+													<td key={`category`}>{moveInfo.category_id}</td>
+													<td key={`power`}>{moveInfo.power}</td>
+													<td key={`accuracy`}>{moveInfo.accuracy}</td>
+													<td key={`pp`}>{moveInfo.pp}</td>
+													<td key={`effect`}>{moveInfo.effect}</td>
+													</>
+												))}
 											</tr>
 										))}
 									</tbody>
--- view/src/Navmenu.js
+++ view/src/Navmenu.js
@@ -4,12 +4,12 @@ import React from 'react'
 export default function Navmenu() {
 	return (
 			<div className="navmenu" key="navmenu">
-				<h1>Navmenu</h1>
-				<ul className="navmenu-items">
-					<li><a href="/pokemon">Pokemon</a></li>
-					<li><a href="/search">Search</a></li>
-					<li><a href="/teams">Teams</a></li>
-					<li><a href="/myaccount">Account</a></li>
+				<h1 key={'h1'}>Navmenu</h1>
+				<ul className="navmenu-items" key={'ul'}>
+					<li key={'pokemon'}><a href="/pokemon" key={'link'}>Pokemon</a></li>
+					<li key={'search'}><a href="/search" key={'link'}>Search</a></li>
+					<li key={'teams'}><a href="/teams" key={'link'}>Teams</a></li>
+					<li key={'myaccount'}><a href="/myaccount" key={'link'}>Account</a></li>
 				</ul>
 			</div>
 	)
