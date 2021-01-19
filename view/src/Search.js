86fdf94d28e83b92c6fb78beb78c15209bfaa986
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2021-01-19 15:24:09 -0500

    Remove unused useMemo import from Search.js

--- view/src/Search.js
+++ view/src/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 // Components:
