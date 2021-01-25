5c8289e9e030439169877d3d4f66f8d58178401b
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2021-01-25 14:49:55 -0500

    Create Signup and Signin forms

--- view/src/Signup.js
+++ view/src/Signup.js
@@ -2,6 +2,24 @@ import React, { useEffect, useState } from "react";
 
 export default function Signup () {
 	return (
+		<>
 		<h1>Sign up</h1>
+		<form action="/signin" method="POST">
+			<div>
+				<label for="name">Username</label>
+				<input type="text" id="username" name="username" required />
+			</div>
+			<div>
+				<label for="name">Email</label>
+				<input type="text" id="email" name="email" required />
+			</div>
+			<div>
+				<label for="name">Password</label>
+				<input type="text" id="password" name="password" required />
+			</div>
+			<button>Sign up</button>
+		</form>
+		<a href="/signin">Sign in</a>
+		</>
 	)
 }
