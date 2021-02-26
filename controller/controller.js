b7052f66595ebbf2f7aac57bd9d84efa8b9070f1
Mike Koscinski <59475481+mikekoscinski@users.noreply.github.com>
2021-02-26 11:38:24 -0500

    Add double backticks, single-quotes, double-quotes for vim syntax highlighting (single characters that indicate text caused entire program to appear in text syntax color)

--- controller/controller.js
+++ controller/controller.js
@@ -42,7 +42,7 @@ const generateAccessToken = (user) => {
 // Utility functions:
 const passwordIsValid = (password) => {
 	return (
-		password.match(/^(?=.*?[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[`~!@#$%^&*()\-_=+\[\]{}\\|:;'"<>,.\/?]).{12,100}$/g)
+		password.match(/^(?=.*?[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[``~!@#$%^&*()\-_=+\[\]{}\\|:;''""<>,.\/?]).{12,100}$/g)
 	)
 }
 // TODO: Stone said could just return this instead of the if
